fc71100b134e1c1aa9a71e24a81859b1bb614825
SNQ-2001 <taishin05109@gmail.com>
2022-06-01 15:17:55 +0900

    first commit

---
+++ JailBreak-Check-Demo/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  JailBreak-Check-Demo
+//
+//  Created by TAISHIN MIYAMOTO on 2022/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var Jailbreak: Bool = false
+    var body: some View {
+        Text(Jailbreak ? "❌: 脱獄あり" : "✅: 脱獄なし")
+            .onAppear() {
+                #if !targetEnvironment(simulator)
+                isDeviceJailBroken1()
+                isDeviceJailBroken2()
+                #endif
+            }
+    }
+    // 脱獄した際に生成されるファイルが存在するか確認
+    func isDeviceJailBroken1() {
+        if access("/Applications/Cydia.app", F_OK) != -1 || access("/Applications/blackra1n.app", F_OK) != -1 || access("/Applications/FakeCarrier.app", F_OK) != -1 || access("/Applications/Icy.app", F_OK) != -1 || access("/Applications/IntelliScreen.app", F_OK) != -1 || access("/Applications/MxTube.app", F_OK) != -1 || access("/Applications/RockApp.app", F_OK) != -1 || access("/Applications/SBSettings.app", F_OK) != -1 || access("/Applications/WinterBoard.app", F_OK) != -1 || access("/Library/MobileSubstrate/MobileSubstrate.dylib", F_OK) != -1 || access("/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist", F_OK) != -1 || access("/Library/MobileSubstrate/DynamicLibraries/Veency.plist", F_OK) != -1 || access("/private/var/lib/apt", F_OK) != -1 || access("/private/var/lib/cydia", F_OK) != -1 || access("/private/var/mobile/Library/SBSettings/Themes", F_OK) != -1 || access("/private/var/stash", F_OK) != -1 || access("/private/var/tmp/cydia.log", F_OK) != -1 || access("/System/Library/LaunchDaemons/com.ikey.bbot.plist", F_OK) != -1 || access("/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist", F_OK) != -1 || access("/usr/bin/sshd", F_OK) != -1 || access("/usr/libexec/sftp-server", F_OK) != -1 || access("/usr/sbin/sshd", F_OK) != -1 || access("/bin/bash", F_OK) != -1 || access("/etc/apt", F_OK) != -1 {
+            Jailbreak = true
+        }
+        Jailbreak = false
+    }
+    // Cydiaが端末内にあるか確認
+    func isDeviceJailBroken2() {
+        if let url = URL(string: "cydia://package/com.example.package") {
+            if UIApplication.shared.canOpenURL(url) {
+                Jailbreak = true
+            }
+            Jailbreak = false
+        }
+        Jailbreak = false
+    }
+
+}
---
+++ JailBreak-Check-Demo/JailBreak_Check_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JailBreak_Check_DemoApp.swift
+//  JailBreak-Check-Demo
+//
+//  Created by TAISHIN MIYAMOTO on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct JailBreak_Check_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
